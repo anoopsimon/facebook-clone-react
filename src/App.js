ce3496843b28a7d896a88b526d5259c4ddf848de
Anoop Simon <Anoop.Simon@health.vic.gov.au>
2023-08-25 15:08:17 +1000

    added mountebank

--- src/App.js
+++ src/App.js
@@ -1,82 +1,42 @@
 import React, { useState } from 'react';
-import DataTable from 'react-data-table-component';
 import './App.css';
+import config from './config';
 
-const App = () => {
-  const [jsonData, setJsonData] = useState([]);
 
-  const handleFileUpload = event => {
-    const file = event.target.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = function (e) {
-        try {
-          const parsedData = JSON.parse(e.target.result);
-          setJsonData(parsedData);
-        } catch (error) {
-          console.error("Error parsing JSON:", error);
-        }
-      };
-      reader.readAsText(file);
+function App() {
+  const [customerData, setCustomerData] = useState(null);
+
+  const getCustomer = async () => {
+    try {
+      const response = await fetch(`${config.apiUrl}/get-customer?customerId=123`);
+      if (!response.ok) {
+        throw new Error(`Failed to fetch customer. Status: ${response.status}`);
+      }
+      
+      const data = await response.json();
+      console.log('Customer data:', data);
+      setCustomerData(data); // Store the response data in state
+    } catch (error) {
+      console.error('Error fetching customer:', error);
     }
   };
 
-  const handleRowUpdate = (newData, oldData) => {
-    const newDataArray = [...jsonData];
-    const sheetName = Object.keys(newDataArray[oldData.index])[0];
-    const rows = newDataArray[oldData.index][sheetName];
-    rows[oldData.indexInSheet] = newData;
-    setJsonData(newDataArray);
-  };
-
-  const handleRowAdd = (newData) => {
-    const newDataArray = [...jsonData];
-    const sheetName = Object.keys(newDataArray[0])[0]; // Assuming all sheets have the same structure
-    const newSheet = newDataArray[0][sheetName].concat(newData);
-    newDataArray[0][sheetName] = newSheet;
-    setJsonData(newDataArray);
-  };
-
   return (
-    <div className="app">
-      <div className="upload-container">
-        <input
-          type="file"
-          accept=".json"
-          onChange={handleFileUpload}
-        />
-      </div>
-      
-      {jsonData.fileName && <p className="file-name">Uploaded File: {jsonData.fileName}</p>}
-
-      {jsonData.map((sheet, index) => {
-        const sheetName = Object.keys(sheet)[0];
-        const rows = sheet[sheetName];
-        const columns = rows.length > 0 ? Object.keys(rows[0]) : [];
-        
-        return (
-          <div key={index}>
-            <h2>{sheetName}</h2>
-            <DataTable
-              columns={columns.map(column => ({
-                name: column,
-                selector: column,
-                sortable: true,
-                editable: true,
-              }))}
-              data={rows}
-              onRowUpdate={handleRowUpdate}
-              onRowAdd={handleRowAdd}
-              selectableRows
-              pagination
-              highlightOnHover
-              dense
-            />
+    <div className="App">
+      <header className="App-header">
+        <h1 style={{textAlign:'center',color:'blue'}}>{config.appTitle}</h1>
+        <div>
+          <button onClick={getCustomer}>Get Customer</button>
+        </div>
+        {customerData && (
+          <div>
+            <h2>Customer Data:</h2>
+            <pre>{JSON.stringify(customerData, null, 2)}</pre>
           </div>
-        );
-      })}
+        )}
+      </header>
     </div>
   );
-};
+}
 
 export default App;
---
+++ src/config.js
@@ -0,0 +1,9 @@
+// config.js
+const config = 
+{
+    apiUrl: 'http://localhost:3001', //replace this with real url in prod
+    appTitle:'The StubHub.'
+  };
+  
+  module.exports = config;
+  
